04d7781315b320b1094d1558b8f79990ee296342
ShivamP-97 <shivampathak19972@gmail.com>
2025-12-03 22:09:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
-// complete the js code
 const fetchButton = document.getElementById("fetch-button");
 const resultsDiv = document.getElementById("results");
 
 const cache = new Map();
+const cacheKey = "triviaData"; 
+const CACHE_DURATION = 60 * 1000; 
 
 const fetchData = async () => {
-  // implement the caching here and store data in cache variable
+  const now = Date.now();
+
+  if (cache.has(cacheKey)) {
+    const cachedEntry = cache.get(cacheKey);
+
+    if (now - cachedEntry.timestamp < CACHE_DURATION) {
+      console.log("Serving data from cache");
+      return cachedEntry.data;
+    }
+  }
 
   console.log("Making API call");
   const response = await fetch("https://opentdb.com/api.php?amount=3");
   const data = await response.json();
 
   cache.set(cacheKey, {
-    timestamp: Date.now(),
+    timestamp: now,
     data: data,
   });
 
